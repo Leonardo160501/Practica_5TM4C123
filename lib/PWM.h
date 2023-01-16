4721155537521cf291b817f1b8359c9290168026
Leonardo160501 <leonardoyo007@outlook.com>
2023-01-16 16:00:51 -0600

    PWM leo

--- lib/PWM.h
+++ lib/PWM.h
@@ -1,7 +1,12 @@
 /*
- * PWM.h
- *
- *  Created on: 16 - Enero - 2023
- *      Author: Leonardo 
- */
+    Fecha: 16/Enero/2023
+    Autor: Leonardo
 
+*/
+
+#ifndef PWM_PWM_H_
+#define PWM_PWM_H_
+
+extern void Configura_Reg_PWM1(uint16_t freq);
+
+#endif
